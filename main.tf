2033e20b39a7316fda05346134bf1a6fa5a346e8
ameetsaha <109667989+ameetsaha@users.noreply.github.com>
2024-06-13 23:41:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-north-1"
+  region = "us-east-1"
 }
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
@@ -16,10 +16,10 @@ module "ec2_instance" {
   name = "t1"
 
   instance_type          = "t3.micro"
-  key_name               = "vaishu"
+  key_name               = "as94054_tf"
   monitoring             = true
-  vpc_security_group_ids = ["sg-03946aa60e5ded4b0"]
-  subnet_id              = "subnet-0bf75427a7b9569dd"
+  vpc_security_group_ids = ["sg-0915d09e8290de738"]
+  subnet_id              = "subnet-06d807c040db6403d"
 
   tags = {
     Terraform   = "true"
